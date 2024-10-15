f67fa87a26baca757b5a3c1bbbd8e725135ae250
abisha <abisha.rajathurai@capecomsolutions.com>
2024-10-15 14:08:26 +0530

    commit for faq

--- src/components/files/FaqSection.js
+++ src/components/files/FaqSection.js
@@ -10,17 +10,17 @@ import {
     {
         id:1,
         title:'How do I choose the right pianica online?',
-        content:'  To select the perfect pianica online, consider your skill level, budget, and desired features. Look for reputable brands, read customer reviews, and choose a model that aligns with your musical preferences.'
+        content:'To select the perfect pianica online, consider your skill level, budget, and desired features. Look for reputable brands, read customer reviews, and choose a model that aligns with your musical preferences.'
     },
     {
         id:2,
         title:'Do you offer warranties on your Pianicas?',
-        content:'  To select the perfect pianica online, consider your skill level, budget, and desired features. Look for reputable brands, read customer reviews, and choose a model that aligns with your musical preferences.'
+        content:'To select the perfect pianica online, consider your skill level, budget, and desired features. Look for reputable brands, read customer reviews, and choose a model that aligns with your musical preferences.'
     },
     {
         id:3,
         title:'What is the best way to maintain and care for my Pianica?',
-        content:'  To select the perfect pianica online, consider your skill level, budget, and desired features. Look for reputable brands, read customer reviews, and choose a model that aligns with your musical preferences.'
+        content:'To select the perfect pianica online, consider your skill level, budget, and desired features. Look for reputable brands, read customer reviews, and choose a model that aligns with your musical preferences.'
     },
   ]
 
@@ -30,10 +30,10 @@ const FaqSection = () => {
     <div className='container mx-auto '>
      <div className='text-center my-[20px] font-bold text-f26'>Frequently Asked Questions</div>
      <div className='flex flex-col justify-center items-center'>
-     <Accordion type="single" collapsible  className="w-full max-w-[75%] ">
+     <Accordion type="single" collapsible defaultValue="1" className="w-full max-w-[75%] ">
        {
         data.map((item)=>(
-            <AccordionItem value={item.id} className='border-2 border-[#D4D9DF] rounded-[8px] p-[10px] mb-[15px]'>
+            <AccordionItem key={item.id}  value={String(item.id)} className='border-2 border-[#D4D9DF] rounded-[8px] px-[15px] mb-[15px] data-[state=open]:border-dm_darkBlue'>
             <AccordionTrigger className='text-f18 font-semibold leading-[22.68px] hover:no-underline '>{item.title}</AccordionTrigger>
             <AccordionContent className='text-f16 font-normal leading-[26px]'>
             {item.content}
